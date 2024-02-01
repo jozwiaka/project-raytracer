f8ad300fc39ecbfafcba1776bdad3ae06e49b970
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-01 20:10:35 +0100

    Update

--- src/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Dev/project-raytracer/src
-# Build directory: /mnt/c/Dev/project-raytracer/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("thirdparty")
