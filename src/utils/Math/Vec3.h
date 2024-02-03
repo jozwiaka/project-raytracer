a69602c414105d8909656b220c45e04498be51dc
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-03 22:13:10 +0100

    Update

--- src/utils/Math/Vec3.h
+++ src/utils/Math/Vec3.h
@@ -46,12 +46,12 @@ public:
         return Vec3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
     }
 
-    float Length() const
+    float Length() const noexcept
     {
         return std::sqrt(x * x + y * y + z * z);
     }
 
-    Vec3 Normalize() const
+    Vec3 Normalize() const noexcept
     {
         float len = Length();
         if (len > 0)
