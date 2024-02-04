c563b6639db6dd9ec4d4822e52dc402b8b63d3a2
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-04 18:40:23 +0100

    Update

---
+++ src/core/Material/Material.cpp
@@ -0,0 +1,3 @@
+#include "Material.h"
+
+Material::Material(const Vec3 &color) : Color(color) {}
---
+++ src/core/Material/Material.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Vec3.h"
+class Material
+{
+public:
+    Vec3 Color;
+
+public:
+    Material() = default;
+    Material(const Vec3 &color);
+};
