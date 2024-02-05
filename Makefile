6cfd5a462d58ce986574d279f4ed09cd23269059
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-05 15:10:02 +0100

    Update

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ prepare:
 	rm -rf build
 	mkdir build
 conan: prepare
-	pip install conan
-	conan profile detect --force # Let Conan try to guess the profile, based on the current operating system and installed tools
-	cat /root/.conan2/profiles/default || echo "Wrong profile path"
-	conan install ./ --output-folder=build --build=missing
+	# pip install conan
+	# conan profile detect --force # Let Conan try to guess the profile, based on the current operating system and installed tools
+	# cat /root/.conan2/profiles/default || echo "Wrong profile path"
+	# conan install ./ --output-folder=build --build=missing
 configure: conan
 	cd build && cmake ..
 build: configure
---
+++ src/vulkan.cpp
@@ -0,0 +1,89 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class VulkanApp
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow *window;
+    VkInstance instance;
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan App";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create Vulkan instance!");
+        }
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+            // Render::Display();
+        }
+    }
+
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main()
+{
+    VulkanApp app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
