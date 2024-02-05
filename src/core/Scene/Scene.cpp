1967e8cabe657c19d19c9ec3d41aaaca783a6688
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-05 10:58:33 +0100

    Update

--- src/core/Scene/Scene.cpp
+++ src/core/Scene/Scene.cpp
@@ -7,7 +7,24 @@ void Scene::AddObject(std::unique_ptr<Object> object)
 
 bool Scene::Intersect(const Ray &ray, Vec3 &hitPoint, Vec3 &normal, Material &material) const
 {
+    float tClosest = std::numeric_limits<float>::infinity();
     bool hit = false;
+
+    for (const auto &object : Objects)
+    {
+        float t;
+        Vec3 objectHitPoint, objectNormal;
+
+        if (object->Intersect(ray, t, objectHitPoint, objectNormal) && t < tClosest)
+        {
+            tClosest = t;
+            hit = true;
+
+            hitPoint = objectHitPoint;
+            normal = objectNormal;
+            material = object->ObjMaterial;
+        }
+    }
 
     return hit;
 }
