0f771bbc72cc2e60eac0a4e0f8cc60c0674764cc
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-05 10:51:01 +0100

    Update

--- src/core/Scene/Scene.cpp
+++ src/core/Scene/Scene.cpp
@@ -1,1 +1,13 @@
 #include "Scene.h"
+
+void Scene::AddObject(std::unique_ptr<Object> object)
+{
+    Objects.push_back(std::move(object));
+}
+
+bool Scene::Intersect(const Ray &ray, Vec3 &hitPoint, Vec3 &normal, Material &material) const
+{
+    bool hit = false;
+
+    return hit;
+}
--- src/core/Scene/Scene.h
+++ src/core/Scene/Scene.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include "Object.h"
+#include "Ray.h"
+#include "Vec3.h"
+#include "Material.h"
 class Scene
 {
 public:
+    std::vector<std::unique_ptr<Object>> Objects;
+
+public:
+    void AddObject(std::unique_ptr<Object> object);
+    bool Intersect(const Ray &ray, Vec3 &hitPoint, Vec3 &normal, Material &material) const;
 };
