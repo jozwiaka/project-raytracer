83e1b7fb22f60b755679d9d594d44901f8745ff9
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-07 16:24:34 +0100

    Update

--- src/core/Object/Cuboid.cpp
+++ src/core/Object/Cuboid.cpp
@@ -16,21 +16,22 @@ Cuboid::Cuboid(const glm::vec3 &center, const glm::vec3 &size, const glm::vec3 &
 bool Cuboid::Intersect(const Ray &ray, float &t, glm::vec3 &hitPoint, glm::vec3 &normal) const
 {
     // xy
-    glm::vec3 p0 = Center + glm::vec3(0, 0, Size.z / 2);
-    glm::vec3 n = glm::vec3(0, 0, 1);
+    auto p0 = Center + glm::vec3(0, 0, Size.z / 2); // point on the plane
+    auto n = glm::vec3(0, 0, 1);                    // normal to the plane
+    auto np0 = glm::dot(n, p0);
 
-    float t1 = (glm::dot(n, p0) - glm::dot(n, ray.Origin)) / glm::dot(n, ray.Direction);
+    float t1 = (np0 - glm::dot(n, ray.Origin)) / glm::dot(n, ray.Direction);
     if (t1 >= 0)
     {
         glm::vec3 p = ray.Origin + t1 * ray.Direction;
 
         if (
-            Math::IsAlmostEqual(glm::dot(n, p), glm::dot(n, p0)) &&
+            Math::IsAlmostEqual(glm::dot(n, p), np0) &&
             Math::IsWithinRange(p.x, Center.x, Size.x / 2) &&
             Math::IsWithinRange(p.y, Center.y, Size.y / 2))
         {
             t = t1;
-            hitPoint = Math::Rot(p, RotationDeg);
+            hitPoint = p;
             normal = glm::normalize((hitPoint - Center));
 
             return true;
