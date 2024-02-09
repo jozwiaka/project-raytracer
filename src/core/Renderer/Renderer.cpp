efe07593a84b0f0c9ed0ca062801fd8defa8d15b
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-09 21:16:24 +0100

    Update

--- src/core/Renderer/Renderer.cpp
+++ src/core/Renderer/Renderer.cpp
@@ -26,14 +26,14 @@ void Renderer::Display()
     Material redMaterial(Math::Vec3(1.0f, 0.0f, 0.0f));
     Material blueMaterial(Math::Vec3(0.0f, 0.0f, 1.0f));
     scene.BackgroundColor = Math::Color3(0.0f, 0.0f, 0.0f);
+    // scene.AddLight(std::make_unique<Light>(Math::Vec3(0.0f, 3.0f, 0.0f), Math::Vec3(1.0f, 1.0f, 1.0f)));
     scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, -1000, 0.0f), 1000, blueMaterial));
     scene.AddObject(std::make_unique<Sphere>(Math::Vec3(3.0f, 2.0f, 0.0f), 2.0f, redMaterial));
     scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, 1.0f, 0.0f), 1.0f, redMaterial));
 
-    scene.AddObject(std::make_unique<Cylinder>(Math::Vec3(2.0f, 2.0f, 2.0f), 1.0f, 1.0f, Math::Vec3(0.0f, 0.0f, 0.0f), redMaterial));
+    // scene.AddObject(std::make_unique<Cylinder>(Math::Vec3(0.0f, 0.0f, 1.0f), 1.0f, 1.0f, Math::Vec3(0.0f, 0.0f, 0.0f), redMaterial));
 
-    scene.AddLight(std::make_unique<Light>(Math::Vec3(0.0f, 3.0f, 0.0f), Math::Vec3(1.0f, 1.0f, 1.0f)));
-    Camera camera(Math::Vec3(0.0f, 5.0f, 10), Math::Vec3(0.0f, 0.0f, 0.0f), Math::Vec3(0.0f, 1.0f, 0.0f));
+    Camera camera(Math::Vec3(0.0f, 5.0f, 10.0f), Math::Vec3(0.0f, 0.0f, 0.f), Math::Vec3(0.0f, 1.0f, 0.0f));
 
     glBegin(GL_POINTS);
 
