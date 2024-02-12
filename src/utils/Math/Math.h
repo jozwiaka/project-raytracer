79debddb4182f3eff17bdf3c337a152b8d649769
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-12 09:04:29 +0100

    Update

--- src/utils/Math/Math.h
+++ src/utils/Math/Math.h
@@ -12,13 +12,18 @@ public:
     using Vec3 = glm::vec3;
 
 public:
+    // Vector
     static float Dot(const Vec3 &v1, const Vec3 &v2) noexcept;
     static Vec3 Cross(const Vec3 &v1, const Vec3 &v2) noexcept;
     static float Length(const Vec3 &v) noexcept;
     static Vec3 Normalize(const Vec3 &v) noexcept;
+
+    // Transformation
     static Vec3 Rotate(const Vec3 &v, const Vec3 &rotationDeg) noexcept;
     static Vec3 Translate(const Vec3 &v, const Vec3 &translation) noexcept;
     static Vec3 Transform(const Vec3 &v, const Vec3 &translation, const Vec3 &rotationDeg) noexcept;
+
+    // Comparison
     static bool IsAlmostEqual(float val1, float val2) noexcept;
     static bool IsWithinRange(float val, float center, float radius) noexcept;
 };
