14fe25e7c8db681f39e5d88bbef48207ad13dd6a
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-12 20:39:03 +0100

    Update

--- src/core/Object/Cylinder.cpp
+++ src/core/Object/Cylinder.cpp
@@ -25,7 +25,9 @@ bool Cylinder::Intersect(const Ray &ray, Interval ray_t, HitRecord &rec) const
     float discriminant = half_b * half_b - a * c;
 
     if (discriminant < 0.0f)
+    {
         return false;
+    }
 
     // Find the nearest root that lies in the acceptable range.
     auto sqrtd = sqrt(discriminant);
@@ -34,16 +36,20 @@ bool Cylinder::Intersect(const Ray &ray, Interval ray_t, HitRecord &rec) const
     {
         root = (-half_b + sqrtd) / a;
         if (!ray_t.Surrounds(root))
+        {
             return false;
+        }
     }
     auto p = rayLocal.At(root);
-    if (Math::IsWithinRange(p.y, 0.0f, m_Height / 2.0f))
+    if (!Math::IsWithinRange(p.y, 0.0f, m_Height / 2.0f))
     {
-        rec.t = root;
-        rec.Point = ray.At(rec.t);
-        auto outwardNormal = Math::Normalize(rec.Point - m_Center);
-        rec.SetFaceNormal(ray, outwardNormal);
-        rec.Mat = m_Mat;
-        return true;
+        return false;
     }
+
+    rec.t = root;
+    rec.Point = ray.At(rec.t);
+    auto outwardNormal = Math::Normalize(rec.Point - m_Center);
+    rec.SetFaceNormal(ray, outwardNormal);
+    rec.Mat = m_Mat;
+    return true;
 }
