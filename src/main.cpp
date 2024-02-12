4c52345d4446f5b709cad44a77cc3662a995af4b
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-12 21:25:00 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main()
     // scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, 2.0f, 0.0f), 2.0f, dielectric));
     // scene.AddObject(std::make_unique<Sphere>(Math::Vec3(4.0f, 2.0f, 0.0f), 2.0f, metal));
 
-    scene.AddObject(std::make_unique<Cylinder>(Math::Vec3(2.0f, 2.0f, 0.0f), 2.0f, 4.0f, Math::Vec3(0.0f, 0.0f, 0.0f), metal));
+    scene.AddObject(std::make_unique<Cylinder>(Math::Vec3(2.0f, 1.0f, 0.0f), 2.0f, 2.0f, Math::Vec3(0.0f, 0.0f, 0.0f), metal));
     scene.AddObject(std::make_unique<Cuboid>(Math::Vec3(-2.0f, 1.0f, 0.0f), Math::Vec3(2.0f, 2.0f, 2.0f), Math::Vec3(0.0f, 0.0f, 0.0f), metal));
 
     float aspectRatio = 16.0f / 9.0f;
