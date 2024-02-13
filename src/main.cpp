8a7db118f9e0075b31e2d018845ca6a9d62b553b
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-13 20:46:50 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,69 @@
 #include "Metal.h"
 #include "Dielectric.h"
 #include "Image.h"
+#include "Random.h"
 
 int main()
 {
-    auto lambertian = std::make_shared<Lambertian>(Color(0.3, 0.3, 0.3));
-    auto metal = std::make_shared<Metal>(Color(0.2, 0.2, 0.2), 0.5);
-    auto dielectric = std::make_shared<Dielectric>(1.5);
-
     float aspectRatio = 16.0f / 9.0f;
     int width = 1200;
     Image image(width, aspectRatio);
 
-    auto cameraPosition = Math::Vec3(0.0f, 3.0f, 5.0f);
-    auto cameraTarget = Math::Vec3(0.0f, 2.0f, 0.0f);
+    auto cameraPosition = Math::Vec3(13.0f, 2.0f, 3.0f);
+    auto cameraTarget = Math::Vec3(0.0f, 0.0f, 0.0f);
     auto cameraUpVector = Math::Vec3(0.0f, 1.0f, 0.0f);
-    float defocusAngle = 5.0f;
+    float defocusAngle = 0.0f;
     float verticalFOV = 90.0f;
     Camera camera(cameraPosition, cameraTarget, cameraUpVector, defocusAngle, verticalFOV, &image);
 
     Scene scene;
-    scene.BackgroundColor = Color();
-    // scene.AddLight(std::make_unique<Light>(Math::Vec3(0.0f, 3.0f, 0.0f), Math::Vec3(1.0f, 1.0f, 1.0f)));
-    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, -1000, 0.0f), 1000, lambertian));
-    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(-4.0f, 2.0f, 0.0f), 2.0f, metal));
-    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, 2.0f, 0.0f), 2.0f, metal));
-    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(4.0f, 2.0f, 0.0f), 2.0f, metal));
+    auto groundMat = std::make_shared<Lambertian>(Color(0.5f, 0.5f, 0.5f));
+    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, -1000, 0.0f), 1000, groundMat));
 
-    // scene.AddObject(std::make_unique<Cylinder>(Math::Vec3(4.0f, 2.0f, -4.0f), 2.0f, 4.0f, Math::Vec3(0.0f, 0.0f, 0.0f), metal));
-    // scene.AddObject(std::make_unique<Cuboid>(Math::Vec3(-4.0f, 2.0f, -4.0f), Math::Vec3(4.0f, 4.0f, 4.0f), Math::Vec3(0.0f, 0.0f, 0.0f), metal));
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto chooseMat = Random::RandomFloat();
+            Math::Vec3 center(a + 0.9f * Random::RandomFloat(), 0.2f, b + 0.9f * Random::RandomFloat());
+
+            if (Math::Length(center - Math::Vec3(4.0f, 0.2f, 0.0f)) > 0.9f)
+            {
+                std::shared_ptr<Material> sphereMat;
+
+                if (chooseMat < 0.8f)
+                {
+                    // diffuse
+                    auto albedo = Random::RandomVector() * Random::RandomVector();
+                    sphereMat = std::make_shared<Lambertian>(albedo);
+                    scene.AddObject(std::make_unique<Sphere>(center, 0.2f, sphereMat));
+                }
+                else if (chooseMat < 0.95f)
+                {
+                    // metal
+                    auto albedo = Random::RandomVector(0.5f, 1.0f);
+                    auto fuzz = Random::RandomFloat(0.0f, 0.5f);
+                    sphereMat = std::make_shared<Metal>(albedo, fuzz);
+                    scene.AddObject(std::make_unique<Sphere>(center, 0.2f, sphereMat));
+                }
+                else
+                {
+                    // glass
+                    sphereMat = std::make_shared<Dielectric>(1.5f);
+                    scene.AddObject(std::make_unique<Sphere>(center, 0.2f, sphereMat));
+                }
+            }
+        }
+    }
+
+    auto material1 = std::make_shared<Dielectric>(1.5f);
+    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(0.0f, 1.0f, 0.0f), 1.0f, material1));
+
+    auto material2 = std::make_shared<Lambertian>(Color(0.4f, 0.2f, 0.1f));
+    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(-4.0f, 1.0f, 0.0f), 1.0f, material2));
+
+    auto material3 = std::make_shared<Metal>(Color(0.7f, 0.6f, 0.5f), 0.0f);
+    scene.AddObject(std::make_unique<Sphere>(Math::Vec3(4.0f, 1.0f, 0.0f), 1.0f, material3));
 
     int maxDepth = 20;
     Renderer renderer{&camera, &scene, &image, maxDepth};
