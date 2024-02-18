0c19a1fa5213357c15aea0b86bfb879468266211
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-18 18:57:44 +0100

    Update

--- src/core/Object.cpp
+++ src/core/Object.cpp
@@ -5,7 +5,32 @@ Object::Object(const Math::Vec3 &center, const Math::Vec3 &rotationDeg, std::sha
       m_RotationDeg(rotationDeg),
       m_Mat(material) {}
 
-std::shared_ptr<Material> Object::GetMaterial()
+Math::Vec3 Object::GetCenter() const
+{
+  return m_Center;
+}
+
+void Object::SetCenter(const Math::Vec3 &center)
+{
+  m_Center = center;
+}
+
+Math::Vec3 Object::GetRotationDeg() const
+{
+  return m_RotationDeg;
+}
+
+void Object::SetRotationDeg(const Math::Vec3 &rotationDeg)
+{
+  m_RotationDeg = rotationDeg;
+}
+
+std::shared_ptr<Material> Object::GetMaterial() const
 {
   return m_Mat;
+}
+
+void Object::SetMaterial(const std::shared_ptr<Material> &material)
+{
+  m_Mat = material;
 }
--- src/core/Object.h
+++ src/core/Object.h
@@ -13,7 +13,15 @@ public:
     Object(const Math::Vec3 &center, const Math::Vec3 &rotationDeg, std::shared_ptr<Material> material);
     virtual ~Object() = default;
     virtual bool Intersect(const Ray &ray, Interval ray_t, HitRecord &rec) const = 0;
-    std::shared_ptr<Material> GetMaterial();
+
+    Math::Vec3 GetCenter() const;
+    void SetCenter(const Math::Vec3 &center);
+
+    Math::Vec3 GetRotationDeg() const;
+    void SetRotationDeg(const Math::Vec3 &rotationDeg);
+
+    std::shared_ptr<Material> GetMaterial() const;
+    void SetMaterial(const std::shared_ptr<Material> &material);
 
 protected:
     Math::Vec3 m_Center;
