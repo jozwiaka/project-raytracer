e08509ffd6824b4e201512aa3b46aba1e8fa8d6e
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-19 08:44:42 +0100

    Update

--- src/core/Scene.cpp
+++ src/core/Scene.cpp
@@ -10,7 +10,7 @@ bool Scene::Intersect(const Ray &ray, Interval ray_t, HitRecord &rec) const
 {
     HitRecord tmpRec;
     bool hit = false;
-    float tClosest = Math::Infinity();
+    float tClosest = ray_t.Max;
 
     for (const auto &object : Objects)
     {
@@ -29,41 +29,41 @@ void Scene::SetUpExampleScene()
 {
     auto groundMat = std::make_shared<MaterialLambertian>(Color(0.5f, 0.5f, 0.5f));
     Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(0.0f, -1000, 0.0f), 1000, groundMat));
-    // for (int a = -11; a < 11; a++)
-    // {
-    //     for (int b = -11; b < 11; b++)
-    //     {
-    //         auto chooseMat = Random::RandomFloat();
-    //         Math::Vec3 center(a + 0.9f * Random::RandomFloat(), 0.2f, b + 0.9f * Random::RandomFloat());
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto chooseMat = Random::RandomFloat();
+            Math::Vec3 center(a + 0.9f * Random::RandomFloat(), 0.2f, b + 0.9f * Random::RandomFloat());
 
-    //         if (Math::Length(center - Math::Vec3(4.0f, 0.2f, 0.0f)) > 0.9f)
-    //         {
-    //             std::shared_ptr<Material> sphereMat;
+            if (Math::Length(center - Math::Vec3(4.0f, 0.2f, 0.0f)) > 0.9f)
+            {
+                std::shared_ptr<Material> sphereMat;
 
-    //             if (chooseMat < 0.8f)
-    //             {
-    //                 // diffuse
-    //                 auto albedo = Random::RandomVector() * Random::RandomVector();
-    //                 sphereMat = std::make_shared<MaterialLambertian>(albedo);
-    //                 Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
-    //             }
-    //             else if (chooseMat < 0.95f)
-    //             {
-    //                 // metal
-    //                 auto albedo = Random::RandomVector(0.5f, 1.0f);
-    //                 auto fuzz = Random::RandomFloat(0.0f, 0.5f);
-    //                 sphereMat = std::make_shared<MaterialMetal>(albedo, fuzz);
-    //                 Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
-    //             }
-    //             else
-    //             {
-    //                 // glass
-    //                 sphereMat = std::make_shared<MaterialDielectric>(1.5f);
-    //                 Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
-    //             }
-    //         }
-    //     }
-    // }
+                if (chooseMat < 0.8f)
+                {
+                    // diffuse
+                    auto albedo = Random::RandomVector() * Random::RandomVector();
+                    sphereMat = std::make_shared<MaterialLambertian>(albedo);
+                    Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
+                }
+                else if (chooseMat < 0.95f)
+                {
+                    // metal
+                    auto albedo = Random::RandomVector(0.5f, 1.0f);
+                    auto fuzz = Random::RandomFloat(0.0f, 0.5f);
+                    sphereMat = std::make_shared<MaterialMetal>(albedo, fuzz);
+                    Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
+                }
+                else
+                {
+                    // glass
+                    sphereMat = std::make_shared<MaterialDielectric>(1.5f);
+                    Objects.emplace_back(std::make_unique<ObjectSphere>(center, 0.2f, sphereMat));
+                }
+            }
+        }
+    }
     auto material1 = std::make_shared<MaterialDielectric>(1.5f);
     Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(0.0f, 1.0f, 0.0f), 1.0f, material1));
     auto material2 = std::make_shared<MaterialLambertian>(Color(0.4f, 0.2f, 0.1f));
