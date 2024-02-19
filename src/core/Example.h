3853ea7153f1bd9747fd8d2c0b264152b9232530
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-19 11:32:52 +0100

    Update

--- src/core/Example.h
+++ src/core/Example.h
@@ -40,7 +40,43 @@ public:
         renderer = std::make_shared<Renderer>(image, camera, scene, numSamples, maxDepth, numThreads, tileSize);
     }
 
+    static void Example_SimpleTest(std::shared_ptr<Image> &image, std::shared_ptr<Camera> &camera, std::shared_ptr<Scene> &scene, std::shared_ptr<Renderer> &renderer)
+    {
+        SetUpSimple(image, camera, scene);
+        constexpr uint32_t numSamples = 20;
+        constexpr uint32_t maxDepth = 10;
+        constexpr uint32_t numThreads = 1000;
+        constexpr uint32_t tileSize = 20;
+        renderer = std::make_shared<Renderer>(image, camera, scene, numSamples, maxDepth, numThreads, tileSize);
+    }
+
 private:
+    static void SetUpSimple(std::shared_ptr<Image> &image, std::shared_ptr<Camera> &camera, std::shared_ptr<Scene> &scene)
+    {
+        constexpr float aspectRatio = 16.0f / 9.0f;
+        constexpr uint32_t width = 1200;
+        image = std::make_shared<Image>(width, aspectRatio);
+
+        constexpr auto cameraPosition = Math::Vec3(13.0f, 2.0f, 3.0f);
+        constexpr auto cameraTarget = Math::Vec3(0.0f, 0.0f, 0.0f);
+        constexpr auto cameraUpVector = Math::Vec3(0.0f, 1.0f, 0.0f);
+        constexpr float defocusAngle = 0.6f;
+        constexpr float verticalFOV = 20.0f;
+        constexpr float focusDist = 10.0f;
+        camera = std::make_shared<Camera>(cameraPosition, cameraTarget, cameraUpVector, defocusAngle, verticalFOV, focusDist, image);
+
+        scene = std::make_shared<Scene>();
+        auto groundMat = std::make_shared<MaterialLambertian>(Color(0.5f, 0.5f, 0.5f));
+        scene->Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(0.0f, -1000, 0.0f), 1000, groundMat));
+
+        auto material1 = std::make_shared<MaterialDielectric>(1.5f);
+        scene->Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(0.0f, 1.0f, 0.0f), 1.0f, material1));
+        auto material2 = std::make_shared<MaterialLambertian>(Color(0.4f, 0.2f, 0.1f));
+        scene->Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(-4.0f, 1.0f, 0.0f), 1.0f, material2));
+        auto material3 = std::make_shared<MaterialMetal>(Color(0.7f, 0.6f, 0.5f), 0.0f);
+        scene->Objects.emplace_back(std::make_unique<ObjectSphere>(Math::Vec3(4.0f, 1.0f, 0.0f), 1.0f, material3));
+    }
+
     static void SetUpComplex(std::shared_ptr<Image> &image, std::shared_ptr<Camera> &camera, std::shared_ptr<Scene> &scene)
     {
         constexpr float aspectRatio = 16.0f / 9.0f;
